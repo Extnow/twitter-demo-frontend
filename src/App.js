666ecc15e86d47eaee183569d918584bdb0926eb
Ryabushkin Mikhail <ryabushkin.mikhail@gmail.com>
2018-06-14 00:57:03 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "normalize.css";
+
+class App extends Component {
+  render() {
+    return <div className="App" />;
+  }
+}
+
+export default App;
