38b3075fc78e7e5509dc41e98f1447a56966fb9d
Ryabushkin Mikhail <ryabushkin.mikhail@gmail.com>
2018-07-21 11:35:55 +0300

    fix error

--- src/ProfilePage/UserInfo/index.js
+++ src/ProfilePage/UserInfo/index.js
@@ -61,7 +61,7 @@ type State = {
     reblog: ?string,
     application: Object,
     account: Object,
-    media_attachments: (?Object)[],
+    media_attachments: Array<Object>,
     mentions: (?Object)[],
     tags: (?Object)[],
     emojis: (?Object)[],
